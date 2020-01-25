0cc2aecd281f1f81d5de28d9c6151dc32142d4b5
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-01-25 15:46:03 +0500

    Завершает генерацию данных фотографий

--- js/main.js
+++ js/main.js
@@ -1,18 +1,39 @@
 'use strict';
 
 var PHOTO_AMOUNT = 25;
+var LIKES_MIN = 15;
+var LIKES_MAX = 200;
+var COMMENTS_MIN = 1;
+var COMMENTS_MAX = 15;
+var AVATAR_NUMBER_MIN = 1;
+var AVATAR_NUMBER_MAX = 6;
 var photos = [];
 
-var names = [
+var userNames = [
   'Михаил',
   'Ашот',
   'Мариночка',
   'Влад',
   'Айгуль',
-  'Децл'
+  'Децл',
+  'Женёк',
+  'Мышка'
 ];
 
-var comments = [
+var photoDescriptions = [
+  'На этой неделе прошло торжественное открытие.',
+  'Zombie apocalypse has been successfully prevented.',
+  'Наконец-то получили все данные с Джаваскриптона (ох уж эти космические расстояния)!',
+  'Вот это интрига!',
+  'Уже почти закончился первый месяц 2020-го. Эээй, как так?...',
+  'Паша пришел домой, конец дня.',
+  'Отличный мастер класс! Рекомендую всем!',
+  'Здорово! Прям прониклась.',
+  'Еще больше драйва и веселья по ссылке в шапке профиля.',
+  'What Earth looked like before.'
+];
+
+var userComments = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -21,12 +42,25 @@ var comments = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
+// Генерация случайного числа от min до max включительно
+var getRandomNumber = function (min, max) {
+  return min + Math.floor(Math.random() * (max + 1 - min));
+};
+
+var getMessage = function (number) {
+  var messages = [];
+  for (var i = 0; i < number; i++) {
+    messages.push(userComments[getRandomNumber(0, userComments.length - 1)]);
+  }
+  return messages.join(' ');
+};
+
 // Генерирует комментарий
 var getCommentData = function () {
   return {
-    'avatar': 'img/avatar-6.svg',
-    'message': 'В целом всё неплохо. Но не всё.',
-    'name': 'Артем'
+    'avatar': 'img/avatar-' + getRandomNumber(AVATAR_NUMBER_MIN, AVATAR_NUMBER_MAX) + '.svg',
+    'message': getMessage(getRandomNumber(1, 2)),
+    'name': userNames[getRandomNumber(0, userNames.length - 1)]
   };
 };
 
@@ -44,9 +78,9 @@ var getComments = function (amount) {
 var getPhotoData = function (number) {
   return {
     'url': 'photos/' + number + '.jpg',
-    'description': '',
-    'likes': 2,
-    'comments': getComments(3)
+    'description': photoDescriptions[getRandomNumber(0, photoDescriptions.length - 1)],
+    'likes': getRandomNumber(LIKES_MIN, LIKES_MAX),
+    'comments': getComments(getRandomNumber(COMMENTS_MIN, COMMENTS_MAX))
   };
 };
 
@@ -54,10 +88,9 @@ var getPhotoData = function (number) {
 // Генерирует массив фотографий
 var generatePhotoData = function () {
   for (var i = 0; i < PHOTO_AMOUNT; i++) {
-    var photoData = getPhotoData(i);
+    var photoData = getPhotoData(i + 1);
     photos.push(photoData);
   }
 };
 
 generatePhotoData();
-console.log(photos);
