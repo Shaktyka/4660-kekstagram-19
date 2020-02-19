22d3363094b0c859b30143f4c8e5edeec5f30eff
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-02-19 12:46:56 +0500

    Добавляет файл form.js

---
+++ js/form.js
@@ -0,0 +1,3 @@
+var uploadForm = document.querySelector('.img-upload__form');
+var hashField = uploadForm.querySelector('.text__hashtags');
+var commentField = uploadForm.querySelector('.text__description');
